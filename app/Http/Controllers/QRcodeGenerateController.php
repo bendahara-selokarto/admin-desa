e03d2d92cbe858bc4966daa75a6351a3e1b8224f
harmanto <bendaharadesaselokarto@gmail.com>
2024-11-06 15:55:14 +0700

    add phpword

--- app/Http/Controllers/QRcodeGenerateController.php
+++ app/Http/Controllers/QRcodeGenerateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Maatwebsite\Excel\Facades\Excel;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
@@ -9,6 +10,51 @@ use App\Imports\KeluargaPenerimaManfaatImport;
 
 class QRcodeGenerateController extends Controller
 {
+    public function generateDoc()
+    {
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+        $importedData =  Excel::toCollection(new KeluargaPenerimaManfaatImport, 'file.xlsx')->first();
+
+
+
+        $section = $phpWord->addSection();
+
+
+
+        $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+
+        // $section->addImage("https://www.itsolutionstuff.com/frontTheme/images/logo.png");
+
+        $section->addText($importedData);
+
+
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+
+        try {
+
+            $objWriter->save(storage_path('helloWorld.docx'));
+        } catch (Exception $e) {
+        }
+
+
+
+        return response()->download(storage_path('helloWorld.docx'));
+    }
+
+
     public function import()
     {
         try {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Penyedia;
 use App\Http\Controllers\Controller;
+use Novay\WordTemplate\WordTemplate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\homeController;
@@ -16,6 +17,7 @@ Route::get('/pph23', [homeController::class, 'pph23']);
 Route::get('/pdfStream', [homeController::class, 'pdfStream']);
 Route::get('/code', [QRcodeGenerateController::class, 'qrcode']);
 Route::get('/import', [QRcodeGenerateController::class, 'import']);
+Route::get('/generateDoc', [QRcodeGenerateController::class, 'generateDoc']);
 Route::get('/img', function (Request $request) {
 
     // dd($image);
@@ -28,4 +30,31 @@ Route::get('/img', function (Request $request) {
             $image->toPng()->save('foo.png');
         }
     }
+});
+Route::get('/msword', function () {
+    for ($i = 0; $i < 10; $i++) {
+
+        $file = public_path('storage/surat_pernyataan.rtf');
+
+
+
+
+
+        $array = array(
+            '[NOMOR_SURAT]' => '015/BT/SK/V/2017',
+            '[PERUSAHAAN]' => 'CV. Borneo Teknomedia',
+            '[NAMA]' => 'Melani Malik',
+            '[NIP]' => '6472065508XXXXX',
+            '[ALAMAT]' => 'Jl. Manunggal Gg. 8 Loa Bakung, Samarinda',
+            '[PERMOHONAN]' => 'Permohonan pengurusan pembuatan NPWP',
+            '[KOTA]' => 'Samarinda',
+            '[DIRECTOR]' => 'Noviyanto Rahmadi',
+            '[TANGGAL]' => date('d F Y'),
+        );
+
+        $nama_file = 'surat-keterangan-kerja' . $i . '.doc';
+
+        WordTemplate::export($file, $array, $nama_file);
+    }
+    return;
 });
